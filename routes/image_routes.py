06dd88f9bf84d3c422a5c6912d2795b58e5bb27e
ulasiyorum <20200808049@ogr.akdeniz.edu.tr>
2025-02-25 17:45:14 +0300

    intensity input, algorithm refactor, color bug fix

--- routes/image_routes.py
+++ routes/image_routes.py
@@ -8,14 +8,13 @@ import numpy as np
 router = APIRouter(prefix="/image", tags=["Image Processing"])
 
 @router.post("/sketch")
-async def sketch_image(file: UploadFile = File(...)):
+async def sketch_image(file: UploadFile = File(...), intensity: int = 50):
     image_bytes = await file.read()
     image = Image.open(io.BytesIO(image_bytes))
+    image_format = image.format
     image = np.array(image)
 
-    # Çizim efektini uygula
-    sketch = apply_sketch_filter(image)
-
-    # Görseli PNG formatında geri döndür
-    _, encoded_img = cv2.imencode('.png', sketch)
-    return Response(content=encoded_img.tobytes(), media_type="image/png")
+    sketch = apply_sketch_filter(image, intensity)
+    correct_color = cv2.cvtColor(sketch, cv2.COLOR_BGR2RGB)
+    _, encoded_img = cv2.imencode('.' + image_format, correct_color)
+    return Response(content=encoded_img.tobytes(), media_type=file.content_type)
--- services/filters.py
+++ services/filters.py
@@ -1,10 +1,27 @@
 import cv2
 import numpy as np
 
-def apply_sketch_filter(image: np.ndarray) -> np.ndarray:
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    inverted = cv2.bitwise_not(gray)
-    blurred = cv2.GaussianBlur(inverted, (21, 21), 0)
-    inverted_blurred = cv2.bitwise_not(blurred)
-    sketch = cv2.divide(gray, inverted_blurred, scale=256.0)
-    return sketch
+def apply_sketch_filter(image: np.ndarray, intensity: int) -> np.ndarray:
+
+    if not 0 <= intensity <= 100:
+        raise ValueError("intensity should be between 0 and 100")
+
+    if intensity == 0:
+        return image
+
+    intensity = intensity / 100
+    grayscale = np.array(np.dot(image[..., :3], [0.299, 0.587, 0.114]), dtype=np.uint8)
+    grayscale = np.stack((grayscale,) * 3, axis=-1)
+    inverted = 255 - grayscale
+    blurred = cv2.GaussianBlur(inverted, ksize=(0, 0), sigmaX=5)
+    result = grayscale * 255.0 / (255.0 - blurred)
+    result[result > 255] = 255
+    result[grayscale == 255] = 255
+
+    result = result.astype('uint8')
+
+    if intensity == 1:
+        return result
+
+    result = (image * (1 - intensity) + (result * intensity)).astype('uint8')
+    return result
