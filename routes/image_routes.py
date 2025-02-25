d67b478e67ccfbe7d2b10b0d63910418bafaac41
ulasiyorum <20200808049@ogr.akdeniz.edu.tr>
2025-02-25 16:00:55 +0300

    Update image_routes.py

--- routes/image_routes.py
+++ routes/image_routes.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, Response
 from services.filters import apply_sketch_filter
 from PIL import Image
+import cv2
 import io
 import numpy as np
 
@@ -17,4 +18,4 @@ async def sketch_image(file: UploadFile = File(...)):
 
     # Görseli PNG formatında geri döndür
     _, encoded_img = cv2.imencode('.png', sketch)
-    return {"filename": file.filename, "image": encoded_img.tobytes()}
+    return Response(content=encoded_img.tobytes(), media_type="image/png")
