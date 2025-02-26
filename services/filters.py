713d85fbb7122af219cf5188f5a047c908d6bab6
ulasiyorum <20200808049@ogr.akdeniz.edu.tr>
2025-02-26 22:01:42 +0300

    paper background

--- services/filters.py
+++ services/filters.py
@@ -3,6 +3,8 @@ import numpy as np
 
 def apply_sketch_filter(image: np.ndarray, intensity: int) -> np.ndarray:
 
+
+    paper_texture = cv2.imread('services/texture/paper_texture.jpg', cv2.IMREAD_GRAYSCALE)
     if not 0 <= intensity <= 100:
         raise ValueError("intensity should be between 0 and 100")
 
@@ -14,7 +16,7 @@ def apply_sketch_filter(image: np.ndarray, intensity: int) -> np.ndarray:
 
     sketch_lines = cv2.adaptiveThreshold(grayscale, 255,
                                          cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                         cv2.THRESH_BINARY, 9, 2)
+                                         cv2.THRESH_BINARY, 9, 5)
 
     inverted = 255 - grayscale
 
@@ -23,6 +25,11 @@ def apply_sketch_filter(image: np.ndarray, intensity: int) -> np.ndarray:
     sketch_shading = cv2.divide(grayscale, 255 - blurred, scale=256)
 
     result = cv2.bitwise_and(sketch_shading, sketch_lines)
+
+    paper_texture = cv2.resize(paper_texture, (result.shape[1], result.shape[0]))
+
+    result = cv2.multiply(result, paper_texture, scale=1 / 255.0)
+
     result = result.astype('uint8')
 
     if intensity == 1:
